6061302a03f590360985f243542f6e3bb4fe13c4
Kirill Mesnyankin <mesk@1c.ru>
2017-03-06 15:09:58 +0300

    Apply JScript babel transformer

--- dist/react-sticky.js
+++ dist/react-sticky.js
@@ -513,28 +513,32 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-	var Channel = function Channel(data) {
-	  (0, _classCallCheck3["default"])(this, Channel);
+	var Channel = function () {
+	  function Channel(data) {
+	    (0, _classCallCheck3["default"])(this, Channel);
 
-	  var listeners = [];
-	  data = data || {};
+	    var listeners = [];
+	    data = data || {};
 
-	  this.subscribe = function (fn) {
-	    listeners.push(fn);
-	  };
+	    this.subscribe = function (fn) {
+	      listeners.push(fn);
+	    };
 
-	  this.unsubscribe = function (fn) {
-	    var idx = listeners.indexOf(fn);
-	    if (idx !== -1) listeners.splice(idx, 1);
-	  };
+	    this.unsubscribe = function (fn) {
+	      var idx = listeners.indexOf(fn);
+	      if (idx !== -1) listeners.splice(idx, 1);
+	    };
 
-	  this.update = function (fn) {
-	    if (fn) fn(data);
-	    listeners.forEach(function (l) {
-	      return l(data);
-	    });
-	  };
-	};
+	    this.update = function (fn) {
+	      if (fn) fn(data);
+	      listeners.forEach(function (l) {
+	        return l(data);
+	      });
+	    };
+	  }
+
+	  return Channel;
+	}();
 
 	exports["default"] = Channel;
 
@@ -898,38 +902,58 @@ return /******/ (function(modules) { // webpackBootstrap
 	    return _this;
 	  }
 
-	  Container.prototype.getChildContext = function getChildContext() {
-	    return { 'sticky-channel': this.channel };
-	  };
+	  Container.prototype.getChildContext = function () {
+	    function getChildContext() {
+	      return { 'sticky-channel': this.channel };
+	    }
 
-	  Container.prototype.componentWillMount = function componentWillMount() {
-	    var parentChannel = this.context['sticky-channel'];
-	    if (parentChannel) parentChannel.subscribe(this.updateOffset);
-	  };
+	    return getChildContext;
+	  }();
 
-	  Container.prototype.componentDidMount = function componentDidMount() {
-	    var node = _reactDom2['default'].findDOMNode(this);
-	    this.channel.update(function (data) {
-	      data.node = node;
-	    });
-	  };
+	  Container.prototype.componentWillMount = function () {
+	    function componentWillMount() {
+	      var parentChannel = this.context['sticky-channel'];
+	      if (parentChannel) parentChannel.subscribe(this.updateOffset);
+	    }
 
-	  Container.prototype.componentWillUnmount = function componentWillUnmount() {
-	    this.channel.update(function (data) {
-	      data.node = null;
-	    });
+	    return componentWillMount;
+	  }();
 
-	    var parentChannel = this.context['sticky-channel'];
-	    if (parentChannel) parentChannel.unsubscribe(this.updateOffset);
-	  };
+	  Container.prototype.componentDidMount = function () {
+	    function componentDidMount() {
+	      var node = _reactDom2['default'].findDOMNode(this);
+	      this.channel.update(function (data) {
+	        data.node = node;
+	      });
+	    }
 
-	  Container.prototype.render = function render() {
-	    return _react2['default'].createElement(
-	      'div',
-	      this.props,
-	      this.props.children
-	    );
-	  };
+	    return componentDidMount;
+	  }();
+
+	  Container.prototype.componentWillUnmount = function () {
+	    function componentWillUnmount() {
+	      this.channel.update(function (data) {
+	        data.node = null;
+	      });
+
+	      var parentChannel = this.context['sticky-channel'];
+	      if (parentChannel) parentChannel.unsubscribe(this.updateOffset);
+	    }
+
+	    return componentWillUnmount;
+	  }();
+
+	  Container.prototype.render = function () {
+	    function render() {
+	      return _react2['default'].createElement(
+	        'div',
+	        this.props,
+	        this.props.children
+	      );
+	    }
+
+	    return render;
+	  }();
 
 	  return Container;
 	}(_react2['default'].Component);
@@ -1033,179 +1057,235 @@ return /******/ (function(modules) { // webpackBootstrap
 	    return _this;
 	  }
 
-	  Sticky.prototype.componentWillMount = function componentWillMount() {
-	    this.channel = this.context['sticky-channel'];
-	    this.channel.subscribe(this.updateContext);
-	  };
-
-	  Sticky.prototype.componentDidMount = function componentDidMount() {
-	    var scrollContainerId = this.props.scrollContainerId;
-
-	    var windowEvents = ['resize', 'pageshow', 'load'];
-	    var scrollContainerEvents = ['scroll', 'touchstart', 'touchmove', 'touchend'];
-
-	    if (scrollContainerId) {
-	      this.scrollContainer = document.getElementById(scrollContainerId);
+	  Sticky.prototype.componentWillMount = function () {
+	    function componentWillMount() {
+	      this.channel = this.context['sticky-channel'];
+	      this.channel.subscribe(this.updateContext);
 	    }
 
-	    this.on(window, windowEvents, this.recomputeState);
-	    this.on(this.scrollContainer, scrollContainerEvents, this.recomputeState);
-	    this.recomputeState();
-	  };
+	    return componentWillMount;
+	  }();
 
-	  Sticky.prototype.componentWillReceiveProps = function componentWillReceiveProps() {
-	    this.recomputeState();
-	  };
+	  Sticky.prototype.componentDidMount = function () {
+	    function componentDidMount() {
+	      var scrollContainerId = this.props.scrollContainerId;
 
-	  Sticky.prototype.componentWillUnmount = function componentWillUnmount() {
-	    var windowEvents = ['resize', 'pageshow', 'load'];
-	    var scrollContainerEvents = ['scroll', 'touchstart', 'touchmove', 'touchend'];
+	      var windowEvents = ['resize', 'pageshow', 'load'];
+	      var scrollContainerEvents = ['scroll', 'touchstart', 'touchmove', 'touchend'];
 
-	    this.off(window, windowEvents, this.recomputeState);
-	    this.off(this.scrollContainer, scrollContainerEvents, this.recomputeState);
-	    this.channel.unsubscribe(this.updateContext);
-	  };
-
-	  Sticky.prototype.getXOffset = function getXOffset() {
-	    return this.refs.placeholder.getBoundingClientRect().left;
-	  };
-
-	  Sticky.prototype.getWidth = function getWidth() {
-	    var bounds = this.refs.placeholder.getBoundingClientRect();
-	    return bounds.width || bounds.right - bounds.left;
-	  };
-
-	  Sticky.prototype.getHeight = function getHeight() {
-	    var bounds = _reactDom2['default'].findDOMNode(this.refs.children).getBoundingClientRect();
-	    return bounds.height || bounds.bottom - bounds.top;
-	  };
-
-	  Sticky.prototype.getDistanceFromTop = function getDistanceFromTop() {
-	    return this.refs.placeholder.getBoundingClientRect().top;
-	  };
-
-	  Sticky.prototype.getDistanceFromBottom = function getDistanceFromBottom() {
-	    if (!this.containerNode) return 0;
-	    return this.containerNode.getBoundingClientRect().bottom;
-	  };
-
-	  Sticky.prototype.isSticky = function isSticky() {
-	    if (!this.props.isActive) return false;
-
-	    var fromTop = this.getDistanceFromTop();
-	    var fromBottom = this.getDistanceFromBottom();
-
-	    var topBreakpoint = this.state.containerOffset - this.props.topOffset;
-	    var bottomBreakpoint = this.state.containerOffset + this.props.bottomOffset;
-
-	    return fromTop <= topBreakpoint && fromBottom >= bottomBreakpoint;
-	  };
-
-	  Sticky.prototype.on = function on(target, events, callback) {
-	    events.forEach(function (evt) {
-	      if (!target.addEventListener && target.attachEvent) {
-	        target.attachEvent('on' + evt, callback);
-	      } else {
-	        target.addEventListener(evt, callback);
+	      if (scrollContainerId) {
+	        this.scrollContainer = document.getElementById(scrollContainerId);
 	      }
-	    });
-	  };
 
-	  Sticky.prototype.off = function off(target, events, callback) {
-	    events.forEach(function (evt) {
-	      if (!target.removeEventListener && target.detachEvent) {
-	        target.detachEvent('on' + evt, callback);
-	      } else {
-	        target.removeEventListener(evt, callback);
-	      }
-	    });
-	  };
-
-	  Sticky.prototype.shouldComponentUpdate = function shouldComponentUpdate(newProps, newState) {
-	    var _this2 = this;
-
-	    // Have we changed the number of props?
-	    var propNames = (0, _keys2['default'])(this.props);
-	    if ((0, _keys2['default'])(newProps).length != propNames.length) return true;
-
-	    // Have we changed any prop values?
-	    var valuesMatch = propNames.every(function (key) {
-	      return newProps.hasOwnProperty(key) && newProps[key] === _this2.props[key];
-	    });
-	    if (!valuesMatch) return true;
-
-	    // Have we changed any state that will always impact rendering?
-	    var state = this.state;
-	    if (newState.isSticky !== state.isSticky) return true;
-
-	    // If we are sticky, have we changed any state that will impact rendering?
-	    if (state.isSticky) {
-	      if (newState.height !== state.height) return true;
-	      if (newState.width !== state.width) return true;
-	      if (newState.xOffset !== state.xOffset) return true;
-	      if (newState.containerOffset !== state.containerOffset) return true;
-	      if (newState.distanceFromBottom !== state.distanceFromBottom) return true;
+	      this.on(window, windowEvents, this.recomputeState);
+	      this.on(this.scrollContainer, scrollContainerEvents, this.recomputeState);
+	      this.recomputeState();
 	    }
 
-	    return false;
-	  };
+	    return componentDidMount;
+	  }();
+
+	  Sticky.prototype.componentWillReceiveProps = function () {
+	    function componentWillReceiveProps() {
+	      this.recomputeState();
+	    }
+
+	    return componentWillReceiveProps;
+	  }();
+
+	  Sticky.prototype.componentWillUnmount = function () {
+	    function componentWillUnmount() {
+	      var windowEvents = ['resize', 'pageshow', 'load'];
+	      var scrollContainerEvents = ['scroll', 'touchstart', 'touchmove', 'touchend'];
+
+	      this.off(window, windowEvents, this.recomputeState);
+	      this.off(this.scrollContainer, scrollContainerEvents, this.recomputeState);
+	      this.channel.unsubscribe(this.updateContext);
+	    }
+
+	    return componentWillUnmount;
+	  }();
+
+	  Sticky.prototype.getXOffset = function () {
+	    function getXOffset() {
+	      return this.refs.placeholder.getBoundingClientRect().left;
+	    }
+
+	    return getXOffset;
+	  }();
+
+	  Sticky.prototype.getWidth = function () {
+	    function getWidth() {
+	      var bounds = this.refs.placeholder.getBoundingClientRect();
+	      return bounds.width || bounds.right - bounds.left;
+	    }
+
+	    return getWidth;
+	  }();
+
+	  Sticky.prototype.getHeight = function () {
+	    function getHeight() {
+	      var bounds = _reactDom2['default'].findDOMNode(this.refs.children).getBoundingClientRect();
+	      return bounds.height || bounds.bottom - bounds.top;
+	    }
+
+	    return getHeight;
+	  }();
+
+	  Sticky.prototype.getDistanceFromTop = function () {
+	    function getDistanceFromTop() {
+	      return this.refs.placeholder.getBoundingClientRect().top;
+	    }
+
+	    return getDistanceFromTop;
+	  }();
+
+	  Sticky.prototype.getDistanceFromBottom = function () {
+	    function getDistanceFromBottom() {
+	      if (!this.containerNode) return 0;
+	      return this.containerNode.getBoundingClientRect().bottom;
+	    }
+
+	    return getDistanceFromBottom;
+	  }();
+
+	  Sticky.prototype.isSticky = function () {
+	    function isSticky() {
+	      if (!this.props.isActive) return false;
+
+	      var fromTop = this.getDistanceFromTop();
+	      var fromBottom = this.getDistanceFromBottom();
+
+	      var topBreakpoint = this.state.containerOffset - this.props.topOffset;
+	      var bottomBreakpoint = this.state.containerOffset + this.props.bottomOffset;
+
+	      return fromTop <= topBreakpoint && fromBottom >= bottomBreakpoint;
+	    }
+
+	    return isSticky;
+	  }();
+
+	  Sticky.prototype.on = function () {
+	    function on(target, events, callback) {
+	      events.forEach(function (evt) {
+	        if (!target.addEventListener && target.attachEvent) {
+	          target.attachEvent('on' + evt, callback);
+	        } else {
+	          target.addEventListener(evt, callback);
+	        }
+	      });
+	    }
+
+	    return on;
+	  }();
+
+	  Sticky.prototype.off = function () {
+	    function off(target, events, callback) {
+	      events.forEach(function (evt) {
+	        if (!target.removeEventListener && target.detachEvent) {
+	          target.detachEvent('on' + evt, callback);
+	        } else {
+	          target.removeEventListener(evt, callback);
+	        }
+	      });
+	    }
+
+	    return off;
+	  }();
+
+	  Sticky.prototype.shouldComponentUpdate = function () {
+	    function shouldComponentUpdate(newProps, newState) {
+	      var _this2 = this;
+
+	      // Have we changed the number of props?
+	      var propNames = (0, _keys2['default'])(this.props);
+	      if ((0, _keys2['default'])(newProps).length != propNames.length) return true;
+
+	      // Have we changed any prop values?
+	      var valuesMatch = propNames.every(function (key) {
+	        return newProps.hasOwnProperty(key) && newProps[key] === _this2.props[key];
+	      });
+	      if (!valuesMatch) return true;
+
+	      // Have we changed any state that will always impact rendering?
+	      var state = this.state;
+	      if (newState.isSticky !== state.isSticky) return true;
+
+	      // If we are sticky, have we changed any state that will impact rendering?
+	      if (state.isSticky) {
+	        if (newState.height !== state.height) return true;
+	        if (newState.width !== state.width) return true;
+	        if (newState.xOffset !== state.xOffset) return true;
+	        if (newState.containerOffset !== state.containerOffset) return true;
+	        if (newState.distanceFromBottom !== state.distanceFromBottom) return true;
+	      }
+
+	      return false;
+	    }
+
+	    return shouldComponentUpdate;
+	  }();
 
 	  /*
 	   * The special sauce.
 	   */
 
 
-	  Sticky.prototype.render = function render() {
-	    var placeholderStyle = { paddingBottom: 0 };
-	    var className = this.props.className;
+	  Sticky.prototype.render = function () {
+	    function render() {
+	      var placeholderStyle = { paddingBottom: 0 };
+	      var className = this.props.className;
 
-	    // To ensure that this component becomes sticky immediately on mobile devices instead
-	    // of disappearing until the scroll event completes, we add `transform: translateZ(0)`
-	    // to 'kick' rendering of this element to the GPU
-	    // @see http://stackoverflow.com/questions/32875046
-	    var style = (0, _assign2['default'])({}, { transform: 'translateZ(0)' }, this.props.style);
+	      // To ensure that this component becomes sticky immediately on mobile devices instead
+	      // of disappearing until the scroll event completes, we add `transform: translateZ(0)`
+	      // to 'kick' rendering of this element to the GPU
+	      // @see http://stackoverflow.com/questions/32875046
+	      var style = (0, _assign2['default'])({}, { transform: 'translateZ(0)' }, this.props.style);
 
-	    if (this.state.isSticky) {
-	      var _stickyStyle = {
-	        position: 'fixed',
-	        top: this.state.containerOffset,
-	        left: this.state.xOffset,
-	        width: this.state.width
-	      };
+	      if (this.state.isSticky) {
+	        var _stickyStyle = {
+	          position: 'fixed',
+	          top: this.state.containerOffset,
+	          left: this.state.xOffset,
+	          width: this.state.width
+	        };
 
-	      var bottomLimit = this.state.distanceFromBottom - this.state.height - this.props.bottomOffset;
-	      if (this.state.containerOffset > bottomLimit) {
-	        _stickyStyle.top = bottomLimit;
+	        var bottomLimit = this.state.distanceFromBottom - this.state.height - this.props.bottomOffset;
+	        if (this.state.containerOffset > bottomLimit) {
+	          _stickyStyle.top = bottomLimit;
+	        }
+
+	        placeholderStyle.paddingBottom = this.state.height;
+
+	        className += ' ' + this.props.stickyClassName;
+	        style = (0, _assign2['default'])({}, style, _stickyStyle, this.props.stickyStyle);
 	      }
 
-	      placeholderStyle.paddingBottom = this.state.height;
+	      var _props = this.props,
+	          topOffset = _props.topOffset,
+	          isActive = _props.isActive,
+	          stickyClassName = _props.stickyClassName,
+	          stickyStyle = _props.stickyStyle,
+	          scrollContainerId = _props.scrollContainerId,
+	          bottomOffset = _props.bottomOffset,
+	          onStickyStateChange = _props.onStickyStateChange,
+	          props = (0, _objectWithoutProperties3['default'])(_props, ['topOffset', 'isActive', 'stickyClassName', 'stickyStyle', 'scrollContainerId', 'bottomOffset', 'onStickyStateChange']);
 
-	      className += ' ' + this.props.stickyClassName;
-	      style = (0, _assign2['default'])({}, style, _stickyStyle, this.props.stickyStyle);
+
+	      return _react2['default'].createElement(
+	        'div',
+	        null,
+	        _react2['default'].createElement('div', { ref: 'placeholder', style: placeholderStyle }),
+	        _react2['default'].createElement(
+	          'div',
+	          (0, _extends3['default'])({}, props, { ref: 'children', className: className, style: style }),
+	          this.props.children
+	        )
+	      );
 	    }
 
-	    var _props = this.props,
-	        topOffset = _props.topOffset,
-	        isActive = _props.isActive,
-	        stickyClassName = _props.stickyClassName,
-	        stickyStyle = _props.stickyStyle,
-	        scrollContainerId = _props.scrollContainerId,
-	        bottomOffset = _props.bottomOffset,
-	        onStickyStateChange = _props.onStickyStateChange,
-	        props = (0, _objectWithoutProperties3['default'])(_props, ['topOffset', 'isActive', 'stickyClassName', 'stickyStyle', 'scrollContainerId', 'bottomOffset', 'onStickyStateChange']);
-
-
-	    return _react2['default'].createElement(
-	      'div',
-	      null,
-	      _react2['default'].createElement('div', { ref: 'placeholder', style: placeholderStyle }),
-	      _react2['default'].createElement(
-	        'div',
-	        (0, _extends3['default'])({}, props, { ref: 'children', className: className, style: style }),
-	        this.props.children
-	      )
-	    );
-	  };
+	    return render;
+	  }();
 
 	  return Sticky;
 	}(_react2['default'].Component);
@@ -1230,7 +1310,11 @@ return /******/ (function(modules) { // webpackBootstrap
 	  scrollContainerId: '',
 	  topOffset: 0,
 	  bottomOffset: 0,
-	  onStickyStateChange: function onStickyStateChange() {}
+	  onStickyStateChange: function () {
+	    function onStickyStateChange() {}
+
+	    return onStickyStateChange;
+	  }()
 	};
 	Sticky.contextTypes = {
 	  'sticky-channel': _react2['default'].PropTypes.any
